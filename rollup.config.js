fca047a99806a417e43491b4a603b3d3df3ee5ac
actra-gschuster <opensource@actra.de>
2017-09-22 14:40:31 +0200

    Updated dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,4 @@
 export default {
-    entry:      './dist/locale-service/index.js',
-    dest:       './dist/ng-i18n-aot-module.umd.js',
-    sourceMap:  false,
-    format:     'umd',
-    moduleName: 'ngI18nAotModule',
     external:   [
         '@angular/core',
         'rxjs/BehaviorSubject',
@@ -13,5 +8,12 @@ export default {
         '@angular/core':        'ng.core',
         'rxjs/BehaviorSubject': 'BehaviorSubject',
         'rxjs/Observable':      'Observable'
-    }
+    },
+    input:      './dist/locale-service/index.js',
+    name:       'ngI18nAotModule',
+    output:     {
+        file:   './dist/ng-i18n-aot-module.umd.js',
+        format: 'umd'
+    },
+    sourceMap:  false
 }
